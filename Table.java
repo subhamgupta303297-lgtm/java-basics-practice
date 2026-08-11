7c25a13819aa7a2d5fb436c8cb49e45f1b7f144f
subhamgupta303297-lgtm <subhamgupta303297@gmail.com>
2026-08-11 20:52:37 +0530

    Tables.java
    making an table of the given number to the user to enter any number to give the result of all values

---
+++ Table.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Table{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+       int num = sc.nextInt();
+       int result = num * 2;
+       System.out.println(result);
+      result = num * 3;
+      System.out.println(result);
+       result = num * 4;
+       System.out.println(result);
+       result = num * 5;
+       System.out.println(result);
+       result = num * 6;
+       System.out.println(result);
+       result = num * 7;
+       System.out.println(result);
+       result = num * 8;
+       System.out.println(result);
+       result = num * 9;
+         System.out.println(result);
+       result = num * 10;
+       System.out.println(result);
+
+       sc.close();
+
+
+
+
+    }
+}
